ad33f5ba5035f7d79af084ad6a7e451352e72cb4
TonySD <ahtoxa.b@gmail.com>
2025-06-27 23:41:25 +0300

    Impl: added templates

--- cmd/myserver/main.go
+++ cmd/myserver/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+
 	"github.com/SelfScriptKiddies/tweaker/internal/config"
 )
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	baseTemplate := "web/templates/layouts/base.html"
+	pageTemplate := fmt.Sprintf("web/templates/pages/%s.html", tmpl)
+
+	t, err := template.ParseFiles(baseTemplate, pageTemplate)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.ExecuteTemplate(w, filepath.Base(baseTemplate), data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 func main() {
 	cfg, err := config.Load("config/config.yaml")
@@ -16,11 +36,18 @@ func main() {
 	addr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
 	log.Printf("starting server on %s", addr)
 
-	http.HandleFunc("/", HandleIndex)
-	http.HandleFunc("/lol", HandleLol)
+	// Routes
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "login", nil)
+	})
 
-	// fs := http.FileServer(http.Dir("assets/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "login", nil)
+	})
+
+	// Static files
+	fs := http.FileServer(http.Dir("web/static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
